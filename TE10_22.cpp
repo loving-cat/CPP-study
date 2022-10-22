dcb9d0c4433d19d0f7cf6decdd498989599e3045
Xiao <106512832+loving-cat@users.noreply.github.com>
2022-10-22 17:20:14 +0800

    Add files via upload

---
+++ TE10_22.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+
+////字符型
+//int main()
+//{
+//	//字符型变量创建
+//	char ch = 'b';
+//	cout << ch << endl;
+//	//字符型所占内存大小
+//	cout << sizeof(char) << endl;
+//	//字符型变量常见错误（err）
+//	//char ch2 = "b";//创建字符型变量的时候，要用单引号
+//	//char ch2 = 'abcdef';//创建字符型变量的时候，单引号内只能有一个字符
+//	
+//	//字符型变量对应ASCII编码
+//	//a-97
+//	//A-65
+//	cout << (int)ch << endl;
+//
+//	system("pause");
+//	return 0;
+//
+//}
+
+int main()
+{
+	//转义字符
+
+	//换行符 \n
+	cout << "hello world\n"<<endl;
+
+	//反斜杠	\\
+
+	cout << "\\" << endl;
+
+	//水平制表符 \t		可以整齐的输出数据
+
+	cout << "aaaa\thelloworld" << endl;
+	cout << "aa\thelloworld" << endl;
+	cout << "aaaaaa\thelloworld" << endl;
+
+	system("pause");
+	return 0;
+}
