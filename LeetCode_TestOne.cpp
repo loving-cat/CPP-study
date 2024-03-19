29572427a3664d313434e43723fa5dd31ec789c4
loving-cat <1367680501@qq.com>
2024-03-19 21:48:04 +0800

    msg

--- LeetCode_TestOne.cpp
+++ LeetCode_TestOne.cpp
@@ -32,3 +32,63 @@ public:
         return v;
     }
 };
+//复写数
+class SolutionTwo
+{
+public:
+    void duplicateZeros(vector<int>& arr)
+    {
+        //1.先找到最后一个数
+        int cur = 0, dest = -1, n = arr.size();
+        while (cur < n)
+        {
+            if (arr[cur]) dest++;
+            else dest += 2;
+            if (dest >= n - 1)break;
+            cur++;
+        }
+        //处理边界情况
+        if (dest == n)
+        {
+            arr[n - 1] = 0;
+            cur--; dest -= 2;
+        }
+        //3.从后向前完成复写操作
+        while (cur >= 0)
+        {
+            if (arr[cur]) arr[dest--] = arr[cur--];
+            else
+            {
+                arr[dest--] = 0;
+                arr[dest--] = 0;
+                cur--;
+            }
+        }
+    }
+};
+//面试经典150题
+//合并有序数组
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int p1 = 0, p2 = 0;
+        int sorted[m + n];//创建另外一个数组
+        int cur = 0;
+        while (p1 < m || p2 < n)
+        {
+            if (p1 == m)//当p1先遍历完
+                cur = nums2[p2++];//cur就去先拿nums2[p2]的值，然后++
+            else if (p2 == n)//如果p2先遍历完
+                cur = nums1[p1++];//cur就先去拿nums1[p1]的值，然后++
+            else if (nums1[p1] < nums2[p2])//当nums1[p1]的值小于nums2[p2]的值时
+                cur = nums1[p1++];//cur去拿nums1[p1]的值
+            else//当nums1[p1]>nums2[p2]
+                cur = nums2[p2++];//cur拿到nums2[p2]的值
+            sorted[p1 + p2 - 1] = cur;//用第三个数组sorted 拿到cur内部的值，依次循环 P1+P2-1，因为p1,p2是下标，所以要-1防止越界访问
+        }
+        for (int i = 0; i != m + n; ++i)//通过m+n次遍历，让nums1数组拿到sorted数组内的值然后return nums1；
+        {
+            nums1[i] = sorted[i];
+        }
+    }
+};
